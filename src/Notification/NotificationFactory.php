eb12fa0c9a193a9214aca001ced8bc1a02045392
Pierre Rineau <pierre.rineau@makina-corpus.com>
2015-12-18 15:51:24 +0100

    Switch to Symfony dependency injection container services using the sf_dic module

---
+++ src/Notification/NotificationFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MakinaCorpus\Drupal\APubSub\Notification;
+
+use MakinaCorpus\APubSub\Notification\NotificationService;
+
+class NotificationFactory
+{
+    public static function getNotificationService(NotificationService $instance)
+    {
+        // Register all "Drupal Way (TM)" notification types.
+        $formatterRegistry = $instance->getFormatterRegistry();
+        foreach (notification_type_info_get(APB_INFO_FORMATTER) as $type => $info) {
+            $formatterRegistry->registerType($type, $info);
+        }
+
+        // Register all "Drupal Way (TM)" channel types.
+        $chanTypeRegistry = $instance->getChanTypeRegistry();
+        foreach (notification_type_info_get(APB_INFO_CHAN) as $type => $info) {
+            $chanTypeRegistry->registerType($type, $info);
+        }
+
+        // Allow other modules to interact with the service in order to for
+        // example register contextual subscribers.
+        module_invoke_all('notification_init', $instance);
+
+        return $instance;
+    }
+}
